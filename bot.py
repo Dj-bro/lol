57414348265e8a82f29b7d7127df270858edecd5
Dj-bro <snap.d.j21264@gmail.com>
2025-03-24 15:52:10 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from telethon import TelegramClient, events
 API_ID = 19485675  # <-- Apna API ID yahan daalo
 API_HASH = "14e59046dacdc958e5f1936019fb064b" # <-- Apna API Hash yahan daalo
 BOT_TOKEN = "7944623129:AAERgJq7BtJJL8ihgHA41zriOJW7I0eN1Sc"  # <-- Apna bot token yahan daalo
-GEMINI_API_KEY = "AIzaSyAXTmJbFfFQBU0bFKpswCyfCytoCL7LfLU"  # <-- Gemini API Key
+GEMINI_API_KEY = "AIzaSyA0TFr2rP9CKGb6l7CH4L3uJ3R-LpcCANw"  # <-- Gemini API Key
 JWT_SECRET = secrets.token_urlsafe(32)  # <-- Secret Key for JWT Tokens
 JWT_ALGORITHM = "HS256"
 
